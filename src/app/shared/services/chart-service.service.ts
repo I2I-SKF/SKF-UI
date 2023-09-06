70030c167061935993fda324a3fc58c48fa5698e
jeet <jeet.dank@ideastoimpacts.com>
2023-09-06 18:55:55 +0530

    changes

---
+++ src/app/shared/services/chart-service.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CHART_CONFIGURATIONS } from '../constants/charts';
+import { Subject } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ChartServiceService {
+
+  constructor() { }
+  mySubject = new Subject<string>();
+
+
+
+  updateChart(chart:string,data:any[]){
+      CHART_CONFIGURATIONS[chart].series = data;
+      this.mySubject.next(chart);
+  }
+
+
+
+}
