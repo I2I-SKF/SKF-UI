20d003991c66e8df843d53771e143d774c076400
jeet <jeet.dank@ideastoimpacts.com>
2023-10-09 18:43:13 +0530

    icons updated in assets | ui changes

--- src/app/layouts/constants/navigation.ts
+++ src/app/layouts/constants/navigation.ts
@@ -1,34 +1,36 @@
+
+
 export const NAVIGATION = [
-  { icon: 'home', name: 'Home', link: '/home', requiredPermission: 'user' },
+  { icon: '../../../assets/imges/icons/home.svg', name: 'Home', link: '/home', requiredPermission: 'user' },
   {
-    icon: 'notifications',
+    icon: '../../../assets/imges/icons/home.svg',
     name: 'Alerts',
     link: '/alerts',
     requiredPermission: 'user',
   },
   {
-    icon: 'description',
+    icon: '../../../assets/imges/icons/config.svg',
     name: 'Reports',
     link: '/services',
     requiredPermission: 'user',
   },
-  { icon: 'map', name: 'Sites', link: '/sites', requiredPermission: 'user' },
+  // { icon: 'map', name: 'Sites', link: '/sites', requiredPermission: 'user' },
   {
-    icon: 'account_circle',
+    icon: '../../../assets/imges/icons/users.svg',
     name: 'Users',
     link: '/services',
     requiredPermission: 'user',
   },
-  {
-    icon: 'paid',
-    name: 'Customer',
-    link: '/services',
-    requiredPermission: 'user',
-  },
-  {
-    icon: 'sync_alt',
-    name: 'Integrations',
-    link: '/services',
-    requiredPermission: 'user',
-  },
+  // {
+  //   icon: 'paid',
+  //   name: 'Customer',
+  //   link: '/services',
+  //   requiredPermission: 'user',
+  // },
+  // {
+  //   icon: 'sync_alt',
+  //   name: 'Integrations',
+  //   link: '/services',
+  //   requiredPermission: 'user',
+  // },
 ];
