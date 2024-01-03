5fc94f59883bb599dc359c1b69d8f615e76c8e5f
jeet <jeet.dank@ideastoimpacts.com>
2024-01-03 15:10:38 +0530

    removed everything from main

--- src/app/core/guards/auth.guard.ts
+++
@@ -1,6 +0,0 @@
-import { CanActivateChildFn, Router } from '@angular/router';
-import { LocalStorageService } from 'src/app/shared/services/local-storage.service';
-
-export const authGuard: CanActivateChildFn = (route, state) => {
-  return true;
-};
